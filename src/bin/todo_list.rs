b71cda165f856c807acd086005e28606222be28b
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-05-13 14:48:53 -0300

    Acrescentei loop do menu principal, opção adicionar tarefa e opção sair.

--- src/bin/todo_list.rs
+++ src/bin/todo_list.rs
@@ -1,5 +1,34 @@
-use std::io;
+use rust_small_projs::todolist::{read_input, Task};
 
 fn main() {
-    
+    let mut tasks: Vec<Task> = Vec::new();
+
+    loop {
+        println!("--- Lista de Tarefas ---");
+        println!(r#"
+1. Adicionar tarefa
+2. Listar tarefas
+3. Marcar tarefa como concluída
+4. Remover tarefa
+5. Sair
+        "#);
+
+        let choice = read_input("Escolha uma opção: ");
+
+        match choice.trim() {
+            "1" => {
+                let desc = read_input("Digite a tarefa: ");
+                tasks.push(Task::new(desc));
+                println!("Tarefa adicionada");
+            },
+            "2" =>{},
+            "3" =>{},
+            "4" =>{},
+            "5" => {
+                println!("Saindo...");
+                break;
+            },
+            _ => println!("Opção inválida"),
+        }
+    }
 }
