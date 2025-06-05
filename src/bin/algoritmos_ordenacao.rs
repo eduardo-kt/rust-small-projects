dac7f6d0f27f1b20edb8960cf6eb05997c59687f
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-06-05 08:08:26 -0300

    feat(algoritmos_ordenacao): adiciona algoritmos de ordenação
    - implementa bubble_sort
    - implementa take_the_bigger (extrai maior elemento iterativamente)
    - cria stub para merge_sort

--- src/bin/algoritmos_ordenacao.rs
+++ src/bin/algoritmos_ordenacao.rs
@@ -1,15 +1,44 @@
 
 fn main() {
     let mut lista = [7,4,6,8,102,5,9,22,66,47,200];
-    for _ in lista {
-        for index in 1..lista.len() {
-            if lista[index-1] > lista[index] {
-                let temp = lista[index-1];
-                lista[index -1] = lista[index];
-                lista[index] = temp;
+    let mut list2 = [5,8,124,1,66,12,88,3,11,55,4];
+    bubble_sort(&mut lista);
+    merge_sort(&mut lista);
+    take_the_bigger(&mut list2);
+
+    
+}
+
+fn bubble_sort(lista: &mut [i32]) {
+    let len = lista.len();
+    for _ in 0..len {
+        for idx in 1..len {
+            if lista[idx-1] > lista[idx] {
+                lista.swap(idx - 1, idx);                
             }
         }
     }
     println!("{:?}", lista);
+}
 
+fn merge_sort(_lista: &mut [i32]) {
+
+}
+
+fn take_the_bigger(lista: &mut [i32]) {
+    let mut temp = lista.to_vec();
+    let mut lista_aux = Vec::new();
+
+    while !temp.is_empty() {
+        if let Some(&maior) = temp.iter().max() {
+            if let Some(idx) = temp.iter().position(|&x| x == maior) {
+                temp.remove(idx);
+                lista_aux.push(maior);
+            }
+        }
+    }
+    for (i, val) in lista_aux.iter().enumerate() {
+        lista[i] = *val;
+    }
+    println!("{:?}", lista);
 }
