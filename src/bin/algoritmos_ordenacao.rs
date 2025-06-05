8d6b88760fc940884e5d999468d0534b24c9867d
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-06-05 08:56:58 -0300

    feat(algoritmos_ordenacao): implementa selection_sort e função auxiliar
    - selection_sort: algoritmo de ordenação
    - find_smaller: função auxiliar que identifica index do menor elemento

--- src/bin/algoritmos_ordenacao.rs
+++ src/bin/algoritmos_ordenacao.rs
@@ -2,9 +2,13 @@
 fn main() {
     let mut lista = [7,4,6,8,102,5,9,22,66,47,200];
     let mut list2 = [5,8,124,1,66,12,88,3,11,55,4];
+    let list3 = [32,4,106,1,99,13,27,8,44];
+
     bubble_sort(&mut lista);
     merge_sort(&mut lista);
     take_the_bigger(&mut list2);
+    let sorted = selection_sort(&list3);
+    println!("{:?}", sorted);
 
     
 }
@@ -41,4 +45,29 @@ fn take_the_bigger(lista: &mut [i32]) {
         lista[i] = *val;
     }
     println!("{:?}", lista);
+}
+
+fn _find_smaller(arr: &[i32]) -> Option<usize> {
+    if arr.is_empty() {
+        return None;
+    }
+    let mut smallest_idx = 0;
+    for i in 1..arr.len() {
+        if arr[i] < arr[smallest_idx] {
+            smallest_idx = i;
+        }
+    }
+    Some(smallest_idx)    
+}
+
+fn selection_sort(arr: &[i32]) -> Vec<i32> {
+    let mut arr_copy = arr.to_vec();
+    let mut new_arr = Vec::with_capacity(arr.len());
+
+    while !arr_copy.is_empty() {
+       if let Some(smallest_idx)= _find_smaller(&arr_copy) {
+           new_arr.push(arr_copy.remove(smallest_idx));
+       }
+    }
+    new_arr
 }
