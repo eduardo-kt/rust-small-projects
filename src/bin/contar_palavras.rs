e81d2c682080c51736c067abccffa4377b6d519b
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-06-24 15:32:34 -0300

    feat(contar_palavras): adiciona função listar_contagem_palavras
    - listar_contagem_palavras: recebe frase do usuário no terminal e conta o número de ocorrência de cada palavra.

--- src/bin/contar_palavras.rs
+++ src/bin/contar_palavras.rs
@@ -1,4 +1,21 @@
 use std::io;
+use std::collections::HashMap;
+
+/// Lê entrada do usuário e conta frequência de palavras.
+/// 
+fn listar_contagem_palavras() {
+    println!("Digite uma frase: ");
+    let mut frase = String::new();
+    io::stdin().read_line(&mut frase).expect("Erro ao ler a frase");
+    //frase = frase.to_string();
+    let mut map: HashMap<String, i32> = HashMap::new();
+
+    for word in frase.split_whitespace() {
+        let count = map.entry(word.to_string()).or_insert(0);
+        *count += 1;
+    }
+    println!("{:?}", map);
+}
 
 fn main() {
     println!("Digite uma frase: ");
@@ -7,6 +24,9 @@ fn main() {
     
     let palavras  = contar_palavras(&entrada);
     println!("A frase possui {} palavras", palavras);
+
+
+    listar_contagem_palavras();
 
 }
 
