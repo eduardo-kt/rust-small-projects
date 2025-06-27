48653165e630b097ff62f17965085a75dbebb28f
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-06-27 15:49:22 -0300

    feat(generics_traits): adiciona exemplo de lifetimes

--- src/bin/generics_traits.rs
+++ src/bin/generics_traits.rs
@@ -19,6 +19,14 @@ fn largest<T: PartialOrd>(list: &[T]) -> &T {
     largest
 }
 
+fn longest<'a>(x:&'a str, y:&'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
 #[derive(Debug)]
 struct Point<T, Q> {
     x: T,
@@ -49,4 +57,13 @@ fn main() {
     println!("{} == {}", ponto.x, ponto.y);
     println!("{}: {}", ponto2.x, ponto2.y);
     println!("{}) {}", ponto3.x, ponto3.y);
+
+    // lifetimes
+    let string1 = String::from("abcd");
+    {
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {result}");
+    }
 }
