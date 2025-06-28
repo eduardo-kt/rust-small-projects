07c2fa56f627fbac3f1c9018d5f9a7aec4dc7b2b
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-06-28 11:06:34 -0300

    feat(simple_tests): adiciona teste que falha

--- src/simple_tests.rs
+++ src/simple_tests.rs
@@ -5,9 +5,14 @@ pub fn simple_sum(a: i32, b: i32) -> i32 {
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
     fn simple_sum_2_2() {
         assert_eq!(simple_sum(2,2), 4)
+    }
+
+    #[test]
+    fn simple_sum_fails() {
+        assert_eq!(simple_sum(3, 5),9)
     }
 }
