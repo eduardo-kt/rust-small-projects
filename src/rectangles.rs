862fe7ed36be1063478688bc2129eec8478e1856
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-06-28 11:19:06 -0300

    feat(rectangles): adiciona struct Rectangles e teste para método contains

---
+++ src/rectangles.rs
@@ -0,0 +1,42 @@
+#[derive(Debug)]
+pub struct Rectangle {
+    pub width: u32,
+    pub height: u32,
+}
+
+impl Rectangle {
+    pub fn area(&self) -> u32 {
+        self.height * self.width
+    }
+    pub fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    pub fn contains(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+    
+    pub fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }        
+    }
+
+    pub fn new(width: u32, height:u32) -> Self {
+        Self { width, height }
+    }
+    
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn larger_contains_smaller() {
+        let large = Rectangle::new(23, 12);
+        let smaller = Rectangle::new(7, 10);
+        assert!(large.contains(&smaller));
+    }
+}
