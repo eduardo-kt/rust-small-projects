006258acad6d7a42bcedd454ce47ac5bcb34aa35
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-06-28 12:13:01 -0300

    feat(testing_should_panic): adiciona binário de teste teste de panic esperado

---
+++ src/bin/testing_should_panic.rs
@@ -0,0 +1,10 @@
+//! Binário
+//! Módulo auxiliar com mesmo nome
+//! 
+use rust_small_projs::testing_should_panic::Guess;
+
+fn main() {
+    let one_guess = Guess::new(32);
+    println!("{:?}", one_guess);
+
+}
