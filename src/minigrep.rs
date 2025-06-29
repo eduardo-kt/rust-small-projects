aec98e43c53fca581fe4d66c9e67d09cdadb3b28
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-06-29 16:32:06 -0300

    feat(minigrep): adiciona instrução de leitura de file_path

--- src/minigrep.rs
+++ src/minigrep.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 pub fn run_minigrep() {
     let args: Vec<String> = env::args().collect();
@@ -8,5 +9,10 @@ pub fn run_minigrep() {
 
     println!("Searching for {}", query);
     println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
     
 }
