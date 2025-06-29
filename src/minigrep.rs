abe973bdcac3783e9045e3322de715f53ee91c36
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-06-29 16:17:44 -0300

    feat(minigrep): cria variáveis com o query e o file_path
    -  Listing 12.2 de Klabnick & Nichols (2023)

--- src/minigrep.rs
+++ src/minigrep.rs
@@ -2,5 +2,11 @@ use std::env;
 
 pub fn run_minigrep() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
+    
 }
