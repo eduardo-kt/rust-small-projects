673280afc0ead9e02856f28beb4660b63a56e93e
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-06-29 21:41:02 -0300

    refactor(minigrep): refatora construtor para retornar Result
    - Config::new(): refatora erro para retornar Result
    - let config: refatora para tratar erro e retornar Result<Ok, Err>

--- src/minigrep.rs
+++ src/minigrep.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs;
+use std::process;
 
 pub fn run_minigrep() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -21,14 +25,13 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments");            
         }
-        Config { 
-            query : args[1].clone(),
-            file_path : args[2].clone(),
-        }
-    }
-    
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config {query, file_path})      
+    }    
 }
