57d620bc3ab8818c71d3ff2f7be4cffeaa4d82ff
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-06-30 08:42:22 -0300

    refactor(minigrep): corrige teste unitário falho

--- src/minigrep.rs
+++ src/minigrep.rs
@@ -68,5 +68,11 @@ pub fn search<'a>(
     query: &str,
     contents: &'a str,
 ) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }        
+    }    
+    results
 }
