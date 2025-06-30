e8bc87cfe3620271b1c1cbf361df183d96a1e1fc
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-06-30 08:52:13 -0300

    feat(minigrep): adiciona exemplos no docstring do binário

--- src/bin/minigrep.rs
+++ src/bin/minigrep.rs
@@ -4,6 +4,15 @@
 //! Execute ex(p.245) com: cargo run --bin minigrep needle haystack
 //! 'poem.txt' está em resources.
 //! Execute pag 248 com: cargo run --bin minigrep the resources/poem.txt
+//! # Exemplos
+//! ```bash
+//! # Rodar exemplo que retorna 1 linha
+//! cargo run --bin minigrep frog resources/poem.txt
+//! # Rodar exemplo que retorna multiplas linhas
+//! cargo run --bin minigrep body resources/poem.txt
+//! # Rodar exemplo que não retorna nada
+//! cargo run --bin minigrep monomorphization resources/poem.txt
+//! ```
 
 use rust_small_projs::minigrep::run_minigrep;
 
