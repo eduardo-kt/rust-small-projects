2bcb5aef08b24516804e90ceacf7b80eeb646c7a
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-07-02 10:44:55 -0300

    feat(estruturas): adiciona método inserir()
    - inserir(): adiciona novo cartão na lista encadeada.

--- src/lista_encadeada/estruturas.rs
+++ src/lista_encadeada/estruturas.rs
@@ -1,3 +1,5 @@
+use std::io::{self, Write};
+
 pub enum Cor {
     Verde,
     Amarelo,
@@ -20,6 +22,52 @@ pub struct ListaSimples {
 impl ListaSimples {
     pub fn new() -> Self {
         Self { head: None, proximo_verde: 1, proximo_amarelo: 201 }
+    }
+
+    pub fn inserir(&mut self) {
+        println!("
+        Escolha a cor do cartão. Digite:
+        1 - Cartão Amarelo(Prioridade)
+        2 - Cartão Verde
+        ");
+        print!("> ");
+        io::stdout().flush().unwrap();
+
+        let mut entrada = String::new();
+        io::stdin().read_line(&mut entrada).unwrap();
+
+        let entrada = entrada.trim().to_lowercase();
+
+        let card = match entrada.as_str() {
+            "1" => {
+                let card = Card {
+                    cor: Cor::Amarelo,
+                    nro: self.proximo_amarelo,
+                };
+                self.proximo_amarelo += 1;
+                card
+            },
+            "2" => {
+                let card = Card {
+                    cor: Cor::Verde,
+                    nro: self.proximo_verde,
+                };
+                self.proximo_verde += 1;
+                card
+            },
+            _ => {
+                println!("Entrada inválida. Retornando ao menu principal...");
+                return;
+            },
+            
+        };
+        let novo_elemento = Box::new(Elemento {
+            dado: card,
+            proximo: self.head.take()
+        });
+
+        self.head = Some(novo_elemento);
+        println!("Cartão inserido com sucesso!");
     }
     
 }
