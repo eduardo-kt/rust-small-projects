277047887b415256ce299022b0616518b3b1f39f
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-07-02 10:20:16 -0300

    refactor(estruturas): modifica ListaSimples para lidar com numeração dos cartões
    - Cartão Amarelo inicia em 201
    - Cartão Verde inicia em 1

--- src/lista_encadeada/estruturas.rs
+++ src/lista_encadeada/estruturas.rs
@@ -13,11 +13,13 @@ pub struct Elemento {
 
 pub struct ListaSimples {
     head: Option<Box<Elemento>>,
+    proximo_verde: u32,
+    proximo_amarelo: u32,
 }
 
 impl ListaSimples {
     pub fn new() -> Self {
-        Self { head: None }
+        Self { head: None, proximo_verde: 1, proximo_amarelo: 201 }
     }
     
 }
