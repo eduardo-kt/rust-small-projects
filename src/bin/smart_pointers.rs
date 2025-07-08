c7d4969172d8d2b095eb9237878603603f6739af
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-07-08 08:32:51 -0300

    refactor(smart_pointer): usa struct MyBox para definir valor de y

--- src/bin/smart_pointers.rs
+++ src/bin/smart_pointers.rs
@@ -1,9 +1,12 @@
 //! Mostra exemplos do capítulo 15 do livro 
 //! Klabnik, S., & Nichols, C. (2023). The Rust programming language (2nd edition). 
+use rust_small_projs::smart_pointers::MyBox;
 
 fn main() {
     let x = 5; // isto é um inteiro
-    let y = &x; // isto é um ponteiro para um inteiro
+    // let y = &x; // ponteiro -> inteiro
+    // let y = Box::new(x); // um box -> cópia de x 
+    let y = MyBox::new(x);
 
     assert_eq!(x,5);
     assert_eq!(*y,5); // * dereference. &i32 vira i32
