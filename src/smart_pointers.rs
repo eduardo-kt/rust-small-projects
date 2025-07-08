41b1b16ae9dab340bdec56ea0ef00c92f162e76d
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-07-08 08:44:38 -0300

    feat(smart_pointers): adiciona trait Deref para MyBox

--- src/smart_pointers.rs
+++ src/smart_pointers.rs
@@ -1,11 +1,21 @@
-//! Implementa 
-//! 
+//! Implementa exemplos do capítulo 15 de
+//! Klabnik, S., & Nichols, C. (2023). The Rust programming language
+use std::ops::Deref;
 
-struct MyBox<T>(T);
+pub struct MyBox<T>(T);
+
 
 impl <T> MyBox<T> {
-    fn new(x:T) -> MyBox<T> {
+    pub fn new(x:T) -> MyBox<T> {
         MyBox(x)
+    }    
+}
+
+impl<T> Deref for MyBox<T>  {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
     }
     
 }
