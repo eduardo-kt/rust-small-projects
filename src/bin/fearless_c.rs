9165c43e52c10675defb856e96ae953d721fd806
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-07-09 07:47:01 -0300

    feat(fearless_c): adiciona exemplo 16-1 p. 355

---
+++ src/bin/fearless_c.rs
@@ -0,0 +1,18 @@
+//! Mostra exemplos do capítulo 16 do livro 
+//! Klabnik, S., & Nichols, C. (2023). The Rust programming language (2nd edition).
+use std:: thread;
+use std::time::Duration;
+fn main() {
+    thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {i} from the spawned thread!");
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {i} frokm the main thread!");
+        thread::sleep(Duration::from_millis(1));
+    }
+
+}
